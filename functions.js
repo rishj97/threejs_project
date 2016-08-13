fd6222513244a12121d80f79986d0b18bc3cef6c
Rishabh Jain <rj2315@ic.ac.uk>
2016-08-13 17:05:31 +0530

    First commit

---
+++ functions.js
@@ -0,0 +1,17 @@
+function pixx() {
+    return 89;
+}
+
+function message(x) {
+    if(x==0) {
+        return "yess do it";
+    }
+    return "hello";
+}
+
+function imgsrc(x) {
+    if(x==0) {
+        return "doggy2.gif";
+    }
+    return "doggy.gif";
+}
